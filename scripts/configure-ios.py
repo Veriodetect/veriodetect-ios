712026cc76e3f1a14ea378f622c2653d617669d4
Veriodetect <tigriscapital@icloud.com>
2026-09-25 12:00:43 +0300

    Update configure-ios.py

--- scripts/configure-ios.py
+++ scripts/configure-ios.py
@@ -6,7 +6,7 @@ path = Path('ios/App/App/Info.plist')
 with path.open('rb') as stream:
     info = plistlib.load(stream)
 info['CFBundleDisplayName'] = 'VerioDetect'
-info['CFBundleShortVersionString'] = '0.7.6'
+info['CFBundleShortVersionString'] = '0.7.8'
 info['CFBundleVersion'] = str(int(os.environ.get('BUILD_NUMBER_BASE', '100')) + int(os.environ.get('PROJECT_BUILD_NUMBER', '1')))
 # The inspected app uses OS HTTPS only; no custom cryptography.
 info['ITSAppUsesNonExemptEncryption'] = False
